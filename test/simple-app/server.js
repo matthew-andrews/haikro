5e8560465ade03c21f26337f1d8d010286ddce0d
Matthew Andrews <matt@mattandre.ws>
2014-11-22 23:58:08 +0000

    Add test for full deployment process

---
+++ test/simple-app/server.js
@@ -0,0 +1,4 @@
+require('http').createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end("Probably the simplest webserver in the world");
+}).listen(process.env.PORT || 3000);
