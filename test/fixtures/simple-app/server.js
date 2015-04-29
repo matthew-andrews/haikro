6d773f90100c42d41ae5fccdfe1e430c387175a9
Richard Still <richard.still@ft.com>
2015-04-29 17:56:35 +0100

    Added an extra route for the worker process to use

--- test/fixtures/simple-app/server.js
+++ test/fixtures/simple-app/server.js
@@ -2,10 +2,28 @@
 var fs = require('fs');
 var dep = "this file wasn't here\n";
 var devDep = "this file wasn't here\n";
-try { dep = fs.readFileSync('./node_modules/dep-test/test.txt'); } catch(e) { }
-try { devDep = fs.readFileSync('./node_modules/dev-dep-test/test.txt'); } catch(e) { }
+var pkg = require('./package.json');
+var workerWasStarted = 'nope';
+var appName = 'haikro-' + pkg.name + '.herokuapp.com';
+
+console.log(pkg);
+try { dep = fs.readFileSync('./node_modules/dep-test/test.txt'); } catch (e) { }
+try { devDep = fs.readFileSync('./node_modules/dev-dep-test/test.txt'); } catch (e) { }
 
 require('http').createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("Probably the simplest webserver in the world\ndep:" + dep + "devDep:" + devDep);
+
+	console.log(req.url);
+	if (req.url === '/workerStarted') {
+		workerWasStarted = 'yarp';
+		res.end("You did it, clever little thing!");
+	} else {
+		res.end("Probably the simplest webserver in the world\ndep:"
+			+ dep
+			+ "devDep:"
+			+ devDep
+			+ "worker:"
+			+ workerWasStarted);
+	}
+	
 }).listen(process.env.PORT || 3000);
