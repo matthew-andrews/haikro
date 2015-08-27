0af8686c1aa8b3f198443537afa0b38fd6d7b9ca
Matthew Andrews <matt@mattandre.ws>
2015-08-28 00:06:08 +0100

    Refactor bin

--- bin/haikro.js
+++ bin/haikro.js
@@ -10,7 +10,7 @@ var deploy = require('../lib/deploy');
 program
 	.command('build')
 	.description('packages an app ready to be put onto Heroku')
-	.option('--program', 'directory to package up')
+	.option('--project [project]', 'directory to package up')
 	.option('--strict', 'be strict about which runtimes haikro can run in')
 	.action(function(opts) {
 		console.log("will build");
@@ -25,11 +25,11 @@ program
 program
 	.command('deploy')
 	.description('uploads and releases app to heroku')
-	.option('--app', 'name of application on Heroku')
-	.option('--commit', 'commit hash to show next to release')
-	.option('--project', 'directory to look for haikro tarball')
+	.option('--app [app]', 'name of application on Heroku')
+	.option('--commit [commit]', 'commit hash to show next to release')
+	.option('--project [project]', 'directory to look for haikro tarball')
 	.action(function(opts) {
-		console.log('will deploy');
+		console.log('will deploy ' + opts.app);
 		deploy({
 			app: opts.app,
 			commit: opts.commit,
@@ -47,3 +47,5 @@ function error(err) {
 function succeed() {
 	console.log('haikro out');
 }
+
+program.parse(process.argv);
